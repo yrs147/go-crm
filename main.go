19d42fe6a83131200b73f6f705c18d727dceb4c9
yrs147 <yashraj14700728@gmail.com>
2023-03-02 13:23:24 +0530

    Imported fmt dependency

--- main.go
+++ main.go
@@ -1,21 +1,24 @@
 package main
 
-import(
+import (
+	"fmt"
+
 	"github.com/gofiber/fiber"
+	"github.com/jinzhu/gorm"
 	"github.com/yrs147/go-crm/database"
 )
 
-func setupRoutes(app *fiber.App){
+func setupRoutes(app *fiber.App) {
 	app.Get(GetLeads)
 	app.Get(GetLead)
 	app.Post(NewLead)
 	app.Delete(DeleteLead)
 }
 
-func initDatabase(){
+func initDatabase() {
 	var err error
-	database.DBConn, err = gorm.Open("sqlite3","leads.db")
-	if err !=nil {
+	database.DBConn, err = gorm.Open("sqlite3", "leads.db")
+	if err != nil {
 		panic("failed to connect database")
 	}
 	fmt.Println("Connection opened to database")
@@ -23,11 +26,11 @@ func initDatabase(){
 	fmt.Println("Database Migrated")
 }
 
-func main(){
+func main() {
 	app := fiber.New()
 	initDatabase()
 	setupRoutes(app)
 	app.Listen(3000)
 	defer database.DBConn.Close()
-	
+
 }
