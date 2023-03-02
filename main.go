4b553fcad3fc8509ebcdf296e3fb9c5deb4ff35f
yrs147 <yashraj14700728@gmail.com>
2023-03-02 15:22:05 +0530

    Fixed errors

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"github.com/jinzhu/gorm"
 	"github.com/yrs147/go-crm/database"
 	"github.com/yrs147/go-crm/lead"
-	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
