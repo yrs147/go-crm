edcd82d379d77abce5b140c2c77422a8366b61e5
yrs147 <yashraj14700728@gmail.com>
2023-03-02 13:39:04 +0530

    Added Dependencies

--- database/database.go
+++ database/database.go
@@ -2,7 +2,7 @@ package database
 
 import(
 	"github.com/jinzhu/gorm"
-	"github.com/jinzhu/gorm/dialects/sqlite"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
 var(
--- lead/lead.go
+++ lead/lead.go
@@ -1,13 +1,18 @@
 package lead
 
-import "github.com/jinzhu/gorm"
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/gofiber/fiber"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"github.com/yrs147/go-crm/database"
 
+)
 type Lead struct{
 	gorm.Model
 	Name string
 	Company string
 	Email string 
-	Phone string
+	Phone int
 }
 
 func main(){
