96a49820a3bb55facbd104fd5399377e3a5bedad
yrs147 <yashraj14700728@gmail.com>
2023-03-02 13:51:59 +0530

    Created NewLead Function

--- lead/lead.go
+++ lead/lead.go
@@ -31,6 +31,14 @@ func GetLead(c *fiber.Ctx){
 }
 
 func NewLead(c *fiber.Ctx){
+	db := database.DBConn
+	lead := new(Lead)
+	if err := c.BodyParser(lead); err !=nil {
+		c.Status(503).Send(err)
+		return
+	}
+	db.Create(&lead)
+	c.JSON(lead)
 	
 }
 
