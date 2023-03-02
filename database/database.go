57c6377b1904ec8977299d99b416bca8274fb269
yrs147 <yashraj14700728@gmail.com>
2023-03-02 13:22:33 +0530

    Added database initilization and connection funtion

---
+++ database/database.go
@@ -0,0 +1,12 @@
+package database
+
+import(
+	"github.com/jinzhu/gorm"
+	"github.com/jinzhu/gorm/dialects/sqlite"
+)
+
+var(
+	DBConn *gorm.DB
+)
+
+
--- main.go
+++ main.go
@@ -2,8 +2,32 @@ package main
 
 import(
 	"github.com/gofiber/fiber"
+	"github.com/yrs147/go-crm/database"
 )
 
+func setupRoutes(app *fiber.App){
+	app.Get(GetLeads)
+	app.Get(GetLead)
+	app.Post(NewLead)
+	app.Delete(DeleteLead)
+}
+
+func initDatabase(){
+	var err error
+	database.DBConn, err = gorm.Open("sqlite3","leads.db")
+	if err !=nil {
+		panic("failed to connect database")
+	}
+	fmt.Println("Connection opened to database")
+	database.DBConn.AutoMigrate(&lead.Lead{})
+	fmt.Println("Database Migrated")
+}
+
 func main(){
+	app := fiber.New()
+	initDatabase()
+	setupRoutes(app)
+	app.Listen(3000)
+	defer database.DBConn.Close()
 	
 }
